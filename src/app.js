d1f6dbae4a0e11b313c86248c401911b1a9c5940
Milly Berst <mildredberst@gmail.com>
2020-01-11 22:44:59 +0100

    Setup Set Expenses actions and test cases

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routes/AppRouter'
 import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -16,5 +17,9 @@ const jxs = (
         <AppRouter/>
     </Provider>
 );
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-ReactDOM.render(jxs, document.getElementById('app'));
+store.dispatch(startSetExpenses()).then(()=>{
+    ReactDOM.render(jxs, document.getElementById('app'));
+});
+
