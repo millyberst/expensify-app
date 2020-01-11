0584792fed9c812e75bd4edb83d0585f931bf905
Milly Berst <mildredberst@gmail.com>
2020-01-12 00:22:48 +0100

    Setup remove expense from firebase and test

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -109,4 +109,19 @@ test('should fetch the expenses from firebase', (done) => {
         done();
     });
 
+});
+test('should remove expenses from firebase', (done)=> {
+    const store = createMockStore({});
+    store.dispatch(startRemoveExpense({id: expenses[0].id})).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: "REMOVE_EXPENSE",
+            id: expenses[0].id
+        })
+        return database.ref(`expenses/${actions[0].id}`).remove();
+    })
+    .then((snapshot)=>{
+        expect(snapshot).toBe(undefined);
+        done();
+    });
 });
